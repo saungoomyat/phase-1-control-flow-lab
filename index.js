a6cf423942d8d9010f69205de801d2ecc0049c1a
root <saung.myat@gmail.com>
2022-02-26 21:14:05 -0500

    commit tree 9b8e4e545d72eb45c069f9fd342c8a964b056d73

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400)
+    return 'This one is on me!';
+  else if (someValue > 2000 && someValue <= 2500)
+    return 'I will gladly take your thirty bucks.';
+  else if (someValue > 2500)
+    return 'No can do.';
+ 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  if (someCity === 'NYC')
+    return 'Ok, sounds good.';
+  else 
+    return 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
+  if (someTip === 'generous')
+    return 'Thank you so much.';
+  else if (someTip === 'not as generous')
+    return 'Thank you.';
+  else
+    return 'Bye.';
 }
